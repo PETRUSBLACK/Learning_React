42a35266c993f766a43eb4c56cd35a09c9b3e9e4
PETRUSBLACK <45966588+PETRUSBLACK@users.noreply.github.com>
2022-02-01 00:36:19 +0100

    fetching API

--- src/App.js
+++ src/App.js
@@ -1,48 +1,28 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters: []
+    }
+  }
 
-class App extends Component {
-  render() {
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())
+    .then( users => this.setState( { monsters:users}))
+  }
+
+
+  render(){
     return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello PETRUS.
-          </p>
-          <button>Change Text</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-    </div>
+          {
+            this.state.monsters.map(monster => <h1 key = {monster.id}> {monster.name}</h1>) 
+          }
+      </div>
     )
   }
 }
